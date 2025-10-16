7bac3c4b7eebcf1a04f5c186060ff1bd4d43461c
Harry Sharma <harrysharma1066@gmail.com>
2025-10-16 19:11:54 +0100

    feat: add async script to inject total posts on top of post heatmap

--- themes/bileygr/assets/js/main.js
+++ themes/bileygr/assets/js/main.js
@@ -1,5 +1,8 @@
 import CalHeatmap from "cal-heatmap";
 import LegendLite from "cal-heatmap/plugins/LegendLite";
+import Tooltip from "cal-heatmap/plugins/Tooltip";
+import CalendarLabel from "cal-heatmap/plugins/CalendarLabel";
+
 async function fetchData(){
     try{
         const response = await fetch("/dates.json");
@@ -15,13 +18,12 @@ async function fetchData(){
 
 async function main(){
     dates = await fetchData()
-    console.log(dates)
     const counts = dates.reduce((acc, date) => {
         acc[date] = (acc[date] || 0) + 1;
         return acc;
     }, {});
     const data = Object.entries(counts).map(([date, value]) => ({ date, value }));
-    console.log(data)
+ 
 
     const cal = new CalHeatmap();
     cal.paint({
@@ -43,10 +45,10 @@ async function main(){
             color: {
                 type: 'threshold',
                 range: [  
-                    "#ecfdf5", // Tailwind bg-green-50
-                    "#86efac", // Tailwind bg-green-300
-                    "#22c55e", // Tailwind bg-green-500
-                    "#14532d", // Tailwind bg-green-950 
+                    "#b9f8cf", // bg-green-200
+                    "#86efac", // bg-green-300
+                    "#22c55e", // bg-green-500
+                    "#14532d", // bg-green-950 
                 ],
                 domain: [0,5,10],
             },
@@ -68,12 +70,58 @@ async function main(){
         [
             LegendLite,
             {
+                includeBlank: false,
                 radius: 3,
                 itemSelector: '#ex-ghDay-legend'
             }
-        ]
+        ],
+        [
+            Tooltip,
+            {
+                enabled: true,
+                text: function (date, value, dayjsDate) {
+                    console.log(value)
+                    if (value){
+                        if (value === 1){
+                            return value + ' Post on ' + dayjsDate.format('dddd, MMMM D, YYYY');
+                        }else{
+                            return value + ' Posts on ' + dayjsDate.format('dddd, MMMM D, YYYY');
+                        }
+                    }else{
+                        return 'No Posts on ' + dayjsDate.format('dddd, MMMM D, YYYY');
+                    }
+                },
+            },
+        ],
+        [
+            CalendarLabel,
+            {
+                width: 30,
+                textAlign: "start",
+                text: () => dayjs.weekdaysShort().map((d, i) => (i % 2 == 0 ? '' : d)),
+                padding: [25, 0, 0, 0],
+            },
+
+        ], 
     ]
     );
 }
+
+async function getNumberOfPostsPerYear(year){
+    dates = await fetchData();
+    years = dates.map(str => str.slice(0,4));
+    filteredYears = years.filter(y => y === year);
+    document.getElementById("contributionYear").textContent = filteredYears.length + " posts in " + year;
+}
+
+async function getNumberOfPosts(){
+    dates = await fetchData();
+    years = dates.map(str => str.slice(0,4));
+    document.getElementById("contributionYear").textContent = years.length + " posts in total"; 
+}
+
+document.addEventListener("DOMContentLoaded", async () =>{
+    await getNumberOfPosts();
+})
 
 main()
